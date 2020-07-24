a9a911f25b02e7eda1b6ff81418536206f5d7e24
ag-68 <ahmethasim@gmail.com>
2020-07-24 21:35:16 +0200

    update

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import time
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "35.228.23.197"
+        self.server = "35.228.185.161"
         #192.168.1.246 35.228.23.197
         self.port = 5555
         self.HEADERSIZE = 8
